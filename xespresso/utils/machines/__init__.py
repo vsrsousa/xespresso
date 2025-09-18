c8f59aff1f85e65f94b41244512983e864c9aae2
vsrsousa <38035761+vsrsousa@users.noreply.github.com>
2025-09-17 21:35:49 -0300

    Update __init__.py

--- xespresso/utils/machines/__init__.py
+++ xespresso/utils/machines/__init__.py
@@ -4,5 +4,5 @@ machines package initializer.
 Exposes top-level config and resource utilities.
 """
 
-from .config import load_machine, create_machine
+from .config import load_machine, create_machine, edit_machine
 #from .resources import set_partition, set_serial_partition, set_gpu, set_debug_mode
